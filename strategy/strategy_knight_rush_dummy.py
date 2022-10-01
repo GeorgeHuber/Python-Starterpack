21e32c33641dea1ce314b743b49f58178e71f7cd
George Huber <36140322+GeorgeHuber@users.noreply.github.com>
2022-10-01 14:32:48 -0500

    update bash and num_others in center to 1

--- strategy/strategy_knight_rush_dummy.py
+++ strategy/strategy_knight_rush_dummy.py
@@ -10,8 +10,9 @@ from strategy.strategy import Strategy
 
 import logging
 
-class Strategy_Knight_Rush_But_Better(Strategy):
+class Strategy_Knight_Rush_Dummy(Strategy):
     start_positions = [(0, 0), (9, 0), (9, 9), (0, 9)]
+
     def strategy_initialize(self, my_player_index: int):
         return game.character_class.CharacterClass.KNIGHT
 
@@ -20,27 +21,9 @@ class Strategy_Knight_Rush_But_Better(Strategy):
         current_position = my_player.position
         speed_remaining = my_player.stat_set.speed
 
-        #buy hunting rifle with enough gold 
-        if (current_position.x, current_position.y) == self.start_positions[my_player_index] and my_player.gold >= Item.HUNTER_SCOPE.value.cost and my_player.item == Item.NONE:
+        if (current_position.x, current_position.y) == self.start_positions[my_player_index] and my_player.gold >= Item.RALLY_BANNER.value.cost and my_player.item == Item.NONE:
             return current_position
-        #if in danger of dying and in center move back to home
-        
-        #if moving outside circle can escape the range
-        if self.in_center(current_position) and my_player.item == Item.HUNTER_SCOPE:
-            directions = [(0, 2), (2, 0), (0, -2), (-2, 0)]
-            
-            num_others_in_center = sum([self.in_center(player.position) for player in game_state.player_state_list]) - 1
-            if(num_others_in_center>=1):
-                for i in range(0, 4):
-                    if i != my_player_index:
-                        other_player = game_state.player_state_list[i]
-                        if self.in_center(other_player.position):
-                            for direction in directions:
-                                new_position = Position(current_position.x + direction[0], current_position.y + direction[1])
-                                new_distance = self.get_range_distance(new_position, other_player.position)
-                                if (other_player.stat_set.range < new_distance) and (my_player.stat_set.range >= new_distance):
-                                    return new_position
-        
+
         while(speed_remaining >= 0 and not self.in_center(current_position)):
             if current_position.x < 4:
                 current_position.x += 1
@@ -75,8 +58,8 @@ class Strategy_Knight_Rush_But_Better(Strategy):
     def buy_action_decision(self, game_state: GameState, my_player_index: int) -> Item:
         my_player = game_state.player_state_list[my_player_index]
         my_position = my_player.position
-        if (my_position.x, my_position.y) == self.start_positions[my_player_index] and my_player.gold >= Item.HUNTER_SCOPE.value.cost:
-            return Item.HUNTER_SCOPE
+        if (my_position.x, my_position.y) == self.start_positions[my_player_index] and my_player.gold >= Item.RALLY_BANNER.value.cost:
+            return Item.RALLY_BANNER
         return Item.NONE
 
     def use_action_decision(self, game_state: GameState, my_player_index: int) -> bool:
@@ -87,9 +70,3 @@ class Strategy_Knight_Rush_But_Better(Strategy):
 
     def get_range_distance(self, pos1, pos2) -> int:
         return max(abs(pos1.x - pos2.x), abs(pos1.y - pos2.y))
-
-    # if we are currently in range of opponent and can move out of range to one of 2 safe squares
-    def can_escape_range(self, my_player, other_player):
-        current_distance = self.get_range_distance(my_player.position, other_player.position)
-        new_distance = current_distance + 2 #hard coded for knight rn
-        return new_distance > other_player.stat_set.range
