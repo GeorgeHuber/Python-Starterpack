e8d7af61bb321d5b9d49a9905b51a0b993fb16d1
George Huber <36140322+GeorgeHuber@users.noreply.github.com>
2022-09-30 23:11:44 -0500

    Co-authored-by: Grady <GradyGeorgia@users.noreply.github.com> Co-authored-by: Snowman29 <Snowman29@users.noreply.github.com>

--- start-4-python-bots.sh
+++ start-4-python-bots.sh
@@ -1,1 +1,7 @@
-TODO
+#!/bin/bash
+python3 build.py
+wait
+python3 bot.pyz 0 &
+python3 bot.pyz 1 &
+python3 bot.pyz 2 &
+python3 bot.pyz 3 &
--- strategy/strategy_two.py
+++ strategy/strategy_two.py
@@ -12,13 +12,45 @@ class StrategyTwo(Strategy):
         return game.character_class.CharacterClass.KNIGHT
 
     def move_action_decision(self, game_state: GameState, my_player_index: int) -> Position:
-        return game_state.player_state_list[my_player_index].position
+        my_player = game_state.player_state_list[my_player_index]
+        current_position = my_player.position
+        current_x = current_position.x
+        current_y = current_position.y
+        speed_remaining = my_player.stat_set.speed
+        while(speed_remaining >= 0 and not self.in_center(current_x,current_y)):
+            if(current_x < 4):
+                current_x += 1
+            elif(current_x > 5):
+               current_x -= 1
+            elif(current_y > 5):
+                current_y -= 1
+            elif(current_y < 4):
+                current_y += 1
+            speed_remaining -= 1
+        return Position(current_x, current_y)
+
 
     def attack_action_decision(self, game_state: GameState, my_player_index: int) -> int:
-        return Random().randint(0, 3)
+        player_state_list = game_state.player_state_list
+        my_player = player_state_list[my_player_index]
+        my_range = my_player.stat_set.range
+        for i in range(0, 3):
+            if i != my_player_index:
+                other_player = player_state_list[i]
+                distance = self.get_range_distance(my_player.position, other_player.position)
+                if distance <= my_range:
+                    return i
+        return 0
+
 
     def buy_action_decision(self, game_state: GameState, my_player_index: int) -> Item:
         return Item.NONE
 
     def use_action_decision(self, game_state: GameState, my_player_index: int) -> bool:
         return False
+
+    def in_center(self, x, y) -> bool:
+        return (x == 4 or x == 5) and (y == 4 or y == 5)
+
+    def get_range_distance(self, pos1, pos2) -> int:
+        return max(abs(pos1.x - pos2.x), abs(pos1.y - pos2.y))
